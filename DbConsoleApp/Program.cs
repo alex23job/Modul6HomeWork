0303ca7a8c51c540d70bef0ac055040610ec6841
alex23job <alex23job@yandex.ru>
2023-09-07 13:20:02 +0300

    Добавьте файлы проекта.

---
+++ DbConsoleApp/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DbConsoleApp
+{
+    class Program
+    {
+        static string fileName = "data.csv";
+        static void Main(string[] args)
+        {
+            string strInput = "";
+            Console.WriteLine("Справочник\"Сотрудник\"");
+            while (strInput != "0")
+            {
+                Console.WriteLine("\nДля вывода справочника нажмите < 1 >");
+                Console.WriteLine("Для ввода информации о новом сотруднике нажмите < 2 >");
+                Console.WriteLine("Для завершения нажмите < 0 >");
+                strInput = Console.ReadLine();
+                switch(strInput)
+                {
+                    case "0": 
+                        break;
+                    case "1":
+                        PrintList();
+                        break;
+                    case "2":
+                        InputInfo();
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка при вводе команды выбора действия => " + strInput);
+                        break;
+                }
+            }
+        }
+
+        static void PrintList()
+        {
+            string[] records = GetAllRecords(fileName);
+            if (records != null)
+            {
+                string outPatern = "| {0, -3} | {1, -16} | {2, -30} | {3, -4} | {4, -4} | {5, 10} | {6, -20} |";
+                if (records.Length > 0)
+                {
+                    Console.WriteLine("\nСписок сотрудников\n");
+                    Console.WriteLine(outPatern, "ID", "Запись создана", "Ф. И. О.", "Возр", "Рост", "День рожд.", "Город рождения");
+                    foreach(string rec in records)
+                    {
+                        WorkPerson wp = new WorkPerson(rec, '#');
+                        Console.WriteLine(wp.OutFormatString(outPatern));
+                    }
+                }
+            }
+        }
+
+        static void InputInfo()
+        {
+            string[] records = GetAllRecords(fileName);
+            WorkPerson wp = new WorkPerson();
+            wp.ID = (records == null) ? 1 : records.Length + 1;
+
+            Console.WriteLine("\nВвод данных нового сотрудника : \n");
+            Console.WriteLine("Введите фамилию, имя, отчество сотрудника (пример : Иванов Иван Иванович)");
+            wp.FullName = Console.ReadLine();
+            Console.WriteLine("Введите возраст сотрудника :");
+            string strAge = Console.ReadLine();
+            if (int.TryParse(strAge, out int age))
+            {
+                wp.Age = age;
+            }
+            Console.WriteLine("Введите рост сотрудника :");
+            string strHeight = Console.ReadLine();
+            if (int.TryParse(strHeight, out int h))
+            {
+                wp.Height = h;
+            }
+            Console.WriteLine("Введите дату рождения сотрудника (пример : 01.01.1970)");
+            wp.BirthDay = Console.ReadLine();
+            Console.WriteLine("Введите место рождения сотрудника (пример : город Тула)");
+            wp.BirthCity = Console.ReadLine();
+            Console.WriteLine("Введите должность сотрудника (пример : стажёр)");
+            wp.Position = Console.ReadLine();
+
+            if (records == null)
+            {
+                records = new string[1];
+                records[0] = wp.CsvRecord("#");
+            }
+            else
+            {
+                string[] tmp = new string[records.Length + 1];
+                for (int i = 0; i < records.Length; i++)
+                {
+                    tmp[i] = records[i];
+                }
+                tmp[records.Length] = wp.CsvRecord("#");
+                records = tmp; 
+            }
+            SaveAllRecords(fileName, records);
+        }
+
+        static string[] GetAllRecords(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                return File.ReadAllLines(fileName);
+            }
+            return null;
+        }
+
+        static void SaveAllRecords(string fileName, string[] records)
+        {
+            File.WriteAllLines(fileName, records);
+        }
+    }
+}
---
+++ DbConsoleApp/WorkPerson.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbConsoleApp
+{
+    class WorkPerson
+    {
+        public int ID { get; set; }
+        public string DateCreate { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public int Height { get; set; }
+        public string BirthDay { get; set; }
+        public string BirthCity { get; set; }
+        public string Position { get; set; }
+
+        public WorkPerson() 
+        {
+            ID = 0;
+            DateTime dt = DateTime.Now;
+            DateCreate = string.Format("{0:D02}.{1:D02}.{2:D04} {3:D02}:{4:D02}", dt.Day, dt.Month, dt.Year, dt.Hour, dt.Minute);
+            FullName = "";
+            Age = 0;
+            Height = 0;
+            BirthDay = "";
+            BirthCity = "";
+            Position = "";
+        }
+
+        public WorkPerson(string csvStr, char sep = ';')
+        {
+            string[] zn = csvStr.Split(sep);
+            if (zn.Length >= 8)
+            {
+                if (int.TryParse(zn[0], out int id))
+                {
+                    ID = id;
+                }
+                DateCreate = zn[1];
+                FullName = zn[2];
+                if (int.TryParse(zn[3], out int age))
+                {
+                    Age = age;
+                }
+                if (int.TryParse(zn[4], out int height))
+                {
+                    Height = height;
+                }
+                BirthDay = zn[5];
+                BirthCity = zn[6];
+                Position = zn[7];
+            }
+        }
+
+        public WorkPerson(int id, string fn, int age, int height, string bd, string bc, string pos)
+        {
+            ID = id;
+            DateTime dt = DateTime.Now;
+            DateCreate = string.Format("{0:D02}.{1:D02}.{2:D04} {3:D02}:{4:D02}", dt.Day, dt.Month, dt.Year, dt.Hour, dt.Minute);
+            FullName = fn;
+            Age = age;
+            Height = height;
+            BirthDay = bd;
+            BirthCity = bc;
+            Position = pos;
+        }
+
+        public string CsvRecord(string sep = ";")
+        {
+            StringBuilder res = new StringBuilder(ID.ToString());
+            res.Append(sep + DateCreate);
+            res.Append(sep + FullName);
+            res.Append(sep + Age.ToString());
+            res.Append(sep + Height.ToString());
+            res.Append(sep + BirthDay);
+            res.Append(sep + BirthCity);
+            res.Append(sep + Position);
+            return res.ToString();
+        }
+
+
+        public string OutFormatString(string pattern)
+        {
+            return string.Format(pattern, ID, DateCreate, FullName, Age, Height, BirthDay, BirthCity, Position);
+        }
+        public override string ToString()
+        {
+            StringBuilder res = new StringBuilder("Сотрудник № " + ID.ToString());
+            res.Append(" " + FullName);
+            res.Append(" возраст: " + Age.ToString());
+            res.Append(" должность: " + Position);
+            return res.ToString();
+        }
+    }
+}
